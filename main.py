bff51f46dd6a78ccbce0fd60ec466b747d7933f3
Danil Merinov <danilmerinov9@gmail.com>
2025-04-09 12:19:45 -0400

    added config

---
+++ main.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+
+from alpha_vantage.timeseries import TimeSeries 
+
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+config = {
+    "alpha_vantage": {
+        "key": os.getenv('ALPHA_VANTAGE_API_KEY'), 
+        "symbol": "IBM",
+        "outputsize": "full",
+        "key_adjusted_close": "5. adjusted close",
+    },
+    "data": {
+        "window_size": 20,
+        "train_split_size": 0.80,
+    }, 
+    "plots": {
+        "xticks_interval": 90, 
+        "color_actual": "#001f3f",
+        "color_train": "#3D9970",
+        "color_val": "#0074D9",
+        "color_pred_train": "#3D9970",
+        "color_pred_val": "#0074D9",
+        "color_pred_test": "#FF4136",
+    },
+    "model": {
+        "input_size": 1, # number of feature. start with 1 (close price)
+        "num_lstm_layers": 2,
+        "lstm_size": 32,
+        "dropout": 0.2,
+    },
+    "training": {
+        "device": "cpu", # "cuda" or "cpu"
+        "batch_size": 64,
+        "num_epoch": 100,
+        "learning_rate": 0.01,
+        "scheduler_step_size": 40,
+    }
+}
